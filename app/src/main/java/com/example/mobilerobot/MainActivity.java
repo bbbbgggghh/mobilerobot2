5faf5db29f8bd079a131e95158ac7fdbb05a8a2b
백근호 <51786385+bbbbgggghh@users.noreply.github.com>
2023-05-24 19:03:48 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/mobilerobot/MainActivity.java
+++ app/src/main/java/com/example/mobilerobot/MainActivity.java
@@ -31,6 +31,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //서버에서 센서 신호 받아서 이동, 위치 알림 보내기, 암구호 요청(입력페이지 필요함 intent),
                 //암구호 입력시 홈베이스 이동, 미입력시 촬영 및 따라가기, 관리자 도착시 홈베이스
+                
+                
+                
+                //서버에 야간경비 시작했다고 보냄 -> 인체감지 센서 활성화
+                
+                //서버에서 센서신호 받음
+                robot.goTo("받은 센서위치"); //센서위치로 이동
+                //서버로 테미 위치 보내기
+                
+                //암구호 입력창 인텐트
+                if 암구호 입력 성공
+                    robot.goTo("홈베이스"); //홈베이스로 이동
+                else
+                    robot.beWithMe(); //따라가기
+                    robot.startTelepresence(robot.getAdminInfo().getName(), robot.getAdminInfo().getUserId()); //관리자에게 전화
+                    
+                
+                
             }
         });
     }
